f81d57329e18622b9bef21cf3f6fc466ec543193
diary <105872600+diaryyyy@users.noreply.github.com>
2025-12-06 14:56:15 +0000

    loader

---
+++ loader.lua
@@ -0,0 +1,6 @@
+if game.GameId == 9177123611 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/diaryyyy/Poems/refs/heads/main/games/ZeeHood.lua"))
+else
+    game.Players.LocalPlayer:Kick("universal isnt out yet")
+
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/diaryyyy/Poems/refs/heads/main/loader.lua")
